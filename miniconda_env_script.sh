22628c633d0a28088ae459ed23fbfe70027be0c1
T M Kinnear <50403021+tmkinnear@users.noreply.github.com>
2024-09-24 15:28:47 +0100

    Update miniconda_env_script.sh

--- miniconda_env_script.sh
+++ miniconda_env_script.sh
@@ -22,3 +22,13 @@ conda create --name ${envname} --file spec-file.txt
 #fix missing .so.1 files
 ln -s /home/${USER}/miniconda3/envs/${envname}/lib/libmpi_cxx.so.40.20.1  /home/${USER}/miniconda3/envs/${envname}/lib/libmpi_cxx.so.1
 ln -s /home/${USER}/miniconda3/envs/${envname}/lib/libmpi.so.40.20.2  /home/${USER}/miniconda3/envs/${envname}/lib/libmpi.so.1
+
+source /home/${USER}/.bashrc
+
+mkdir ~/Pluto
+cd ~/Pluto
+PLUTOversion=pluto-4.4-patch3
+wget --no-check-certificate https://plutocode.ph.unito.it/Downloads/${PLUTOversion}.tar.gz #must be updated to correct versions; REALLY should not use "--no-check-certificate"
+tar -xzvf ${PLUTOversion}.tar.gz
+
+PLUTOpath=/home/${USER}/Pluto/PLUTO
